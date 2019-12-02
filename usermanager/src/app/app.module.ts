b500de160222d08d4715adfa7c25b1d5fab0e9ea
Esrakaraks <Esrakaraks>
2019-12-02 13:57:53 +0300

    update

--- usermanager/src/app/app.module.ts
+++ usermanager/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './core/userlist/userlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
